646bd6bdc092eac3b940793ec26cbb0084871cdd
Glyph <glyph@twistedmatrix.com>
2022-05-05 17:05:59 -0700

    clean up into a function

--- src/mopup/__init__.py
+++ src/mopup/__init__.py
@@ -2,9 +2,8 @@ import collections
 from platform import mac_ver
 from re import compile as compile_re
 from sys import version_info
-from typing import Iterable
-from typing import Match
-from typing import re
+from typing import Iterable, Dict
+from typing import Pattern, Match
 from typing import Tuple
 
 import html5lib
@@ -12,52 +11,54 @@ import requests
 from hyperlink import DecodedURL
 
 
-this_mac_ver = tuple(map(int, mac_ver()[0].split(".")[:2]))
-ver = compile_re(r"(\d+)\.(\d+).(\d+)/")
-macpkg = compile_re("python-(.+)-macosx?(.*).pkg")
-
-thismajor, thisminor, thismicro, *other = version_info
-
-# major, minor, micro, macos
-versions = collections.defaultdict(
-    lambda: collections.defaultdict(lambda: collections.defaultdict(dict))
-)
-
-baseurl = DecodedURL.from_text("https://www.python.org/ftp/python/")
-
-
-def alllinksin(u: DecodedURL, e: re) -> Iterable[Tuple[Match, DecodedURL]]:
+def alllinksin(u: DecodedURL, e: Pattern[str]) -> Iterable[Tuple[Match[str], DecodedURL]]:
     for a in html5lib.parse(
         requests.get(u.to_text()).text, namespaceHTMLElements=False
     ).findall(".//a"):
-        match = e.fullmatch(a.text)
+        match = e.fullmatch(a.text or "")
         if match is not None:
             yield match, u.click(a.attrib["href"])
 
 
-for eachver, suburl in alllinksin(baseurl, ver):
-    major, minor, micro = map(int, eachver.groups())
-    print("fetching versions for", major, minor, micro)
-    for eachmac, pkgdl in alllinksin(suburl, macpkg):
-        pyver, macver = eachmac.groups()
-        if pyver == f"{major}.{minor}.{micro}":
-            versions[major][minor][micro][macver] = pkgdl
+def main() -> None:
+    this_mac_ver = tuple(map(int, mac_ver()[0].split(".")[:2]))
+    ver = compile_re(r"(\d+)\.(\d+).(\d+)/")
+    macpkg = compile_re("python-(.+)-macosx?(.*).pkg")
 
-import pprint
+    thismajor, thisminor, thismicro, *other = version_info
 
-pprint.pprint(versions)
-newmicro = max(versions[thismajor][thisminor].keys())
-print("new micro-version:", thismajor, thisminor, newmicro)
-available_mac_vers = versions[thismajor][thisminor][newmicro].keys()
-best_available_mac = max(
-    available_mac_ver
-    for available_mac_ver in available_mac_vers
-    if this_mac_ver >= tuple(int(x) for x in available_mac_ver.split("."))
-)
+    # major, minor, micro, macos
+    versions: Dict[int, Dict[int, Dict[int, Dict[str, DecodedURL]]]] = collections.defaultdict(
+        lambda: collections.defaultdict(lambda: collections.defaultdict(dict))
+    )
 
-print(
-    "update",
-    "needed" if newmicro > thismicro else "not needed",
-    "from",
-    versions[thismajor][thisminor][newmicro][best_available_mac],
-)
+    baseurl = DecodedURL.from_text("https://www.python.org/ftp/python/")
+
+
+
+    for eachver, suburl in alllinksin(baseurl, ver):
+        major, minor, micro = map(int, eachver.groups())
+        print("fetching versions for", major, minor, micro)
+        for eachmac, pkgdl in alllinksin(suburl, macpkg):
+            pyver, macver = eachmac.groups()
+            if pyver == f"{major}.{minor}.{micro}":
+                versions[major][minor][micro][macver] = pkgdl
+
+    import pprint
+
+    pprint.pprint(versions)
+    newmicro = max(versions[thismajor][thisminor].keys())
+    print("new micro-version:", thismajor, thisminor, newmicro)
+    available_mac_vers = versions[thismajor][thisminor][newmicro].keys()
+    best_available_mac = max(
+        available_mac_ver
+        for available_mac_ver in available_mac_vers
+        if this_mac_ver >= tuple(int(x) for x in available_mac_ver.split("."))
+    )
+
+    print(
+        "update",
+        "needed" if newmicro > thismicro else "not needed",
+        "from",
+        versions[thismajor][thisminor][newmicro][best_available_mac],
+    )

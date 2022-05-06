8e31685240528479513876873af99231e4490570
Glyph <glyph@twistedmatrix.com>
2022-05-05 17:57:21 -0700

    coverage?

--- src/mopup/__main__.py
+++ src/mopup/__main__.py
@@ -1,11 +1,13 @@
 """Command-line interface."""
 import click
+from mopup import main as libmain
 
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """MOPUp."""
+    libmain()
 
 
 if __name__ == "__main__":

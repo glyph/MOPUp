efa73a74649028293fc0ca8774c626c0d54aad98
Łukasz Langa <lukasz@langa.pl>
2025-08-17 00:42:47 +0200

    Reorganize destructive test running

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+def test_update_dry_run(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main, ["update", "--dry-run=true"])
     assert result.exit_code == 0
